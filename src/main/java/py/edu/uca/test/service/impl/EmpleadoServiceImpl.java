98eb4cefcfa3f19e3337c3eac6ababc2be312d9b
Ale Feltes <afeltes@sodep.com.py>
2015-09-04 18:08:07 -0400

    Solucion de traer un empleado por id con JPA puro (EntityManager)

--- src/main/java/py/edu/uca/test/service/impl/EmpleadoServiceImpl.java
+++ src/main/java/py/edu/uca/test/service/impl/EmpleadoServiceImpl.java
@@ -1,18 +1,29 @@
 package py.edu.uca.test.service.impl;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import py.edu.uca.test.domain.Empleado;
 import py.edu.uca.test.service.EmpleadoService;
 import py.edu.uca.test.web.dto.EmpleadoDTO;
 
 @Service
 public class EmpleadoServiceImpl implements EmpleadoService {
 
+    @PersistenceContext
+    EntityManager entityManager;
+
     @Override
-    public EmpleadoDTO findById(Long number) {
+    public EmpleadoDTO findById(Long id) {
         EmpleadoDTO dto = new EmpleadoDTO();
-        dto.setNombre("Guardia");
-        // TODO Auto-generated method stub
+        Empleado empleado = entityManager.find(Empleado.class, id);
+        if (empleado != null) {
+            System.out.println("empleado: " + empleado.getNombre());
+            BeanUtils.copyProperties(empleado, dto);
+        }
         return dto;
     }
 
