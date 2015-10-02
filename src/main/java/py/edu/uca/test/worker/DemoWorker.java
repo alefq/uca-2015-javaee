2ef3fdc7cc94c1524274a1a66ee1d13aa60d639c
Ale Feltes <afeltes@sodep.com.py>
2015-10-02 17:32:06 -0400

    Agregamos un System.out para prueba

--- src/main/java/py/edu/uca/test/worker/DemoWorker.java
+++ src/main/java/py/edu/uca/test/worker/DemoWorker.java
@@ -8,24 +8,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-
 @Component("demoWorker")
 public class DemoWorker {
 
     protected static final Logger logger = LoggerFactory.getLogger(DemoWorker.class);
     private static final AtomicLong counter = new AtomicLong(0);
 
-   
     /**
-     * Cada 5 segundos, se procesa la cola de compositions que se encuentren en estado {@link CompositionScrapperStatusEnum#PENDING}
-     * <br/>Una vez invocado el rest service para actualización se marca como "RELOADED".
+     * Cada 5 segundos, se procesa la cola de compositions que se encuentren en
+     * estado {@link CompositionScrapperStatusEnum#PENDING} <br/>
+     * Una vez invocado el rest service para actualización se marca como
+     * "RELOADED".
      */
     @Scheduled(fixedDelay = 5000)
     public void processCompositionsFromQueue() {
         Date now = new Date();
         long increment = counter.incrementAndGet();
         logger.info("[" + increment + "] estoy en el worker, fecha: " + now);
-      
-        }
+        System.out.println("[" + increment + "] estoy en el worker, fecha: " + now);
+    }
 
 }
